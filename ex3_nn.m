d065445f25f37011b469b7735b5378bd2a665245
Ayan Saha <35620678+rock420@users.noreply.github.com>
2019-02-15 20:01:25 +0530

    Update ex3_nn.m

--- ex3_nn.m
+++ ex3_nn.m
@@ -1,20 +1,3 @@
-%% Machine Learning Online Class - Exercise 3 | Part 2: Neural Networks
-
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions 
-%  in this exericse:
-%
-%     lrCostFunction.m (logistic regression cost function)
-%     oneVsAll.m
-%     predictOneVsAll.m
-%     predict.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
 
 %% Initialization
 clear ; close all; clc
@@ -26,9 +9,6 @@ num_labels = 10;          % 10 labels, from 1 to 10
                           % (note that we have mapped "0" to label 10)
 
 %% =========== Part 1: Loading and Visualizing Data =============
-%  We start the exercise by first loading and visualizing the dataset. 
-%  You will be working with a dataset that contains handwritten digits.
-%
 
 % Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
@@ -46,9 +26,7 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% ================ Part 2: Loading Pameters ================
-% In this part of the exercise, we load some pre-initialized 
-% neural network parameters.
-
+% we load some pre-initialized neural network parameters.
 fprintf('\nLoading Saved Neural Network Parameters ...\n')
 
 % Load the weights into variables Theta1 and Theta2
@@ -56,10 +34,7 @@ load('ex3weights.mat');
 
 %% ================= Part 3: Implement Predict =================
 %  After training the neural network, we would like to use it to predict
-%  the labels. You will now implement the "predict" function to use the
-%  neural network to predict the labels of the training set. This lets
-%  you compute the training set accuracy.
-
+%  the labels. 
 pred = predict(Theta1, Theta2, X);
 
 fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
@@ -67,8 +42,6 @@ fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-%  To give you an idea of the network's output, you can also run
-%  through the examples one at the a time to see what it is predicting.
 
 %  Randomly permute examples
 rp = randperm(m);
